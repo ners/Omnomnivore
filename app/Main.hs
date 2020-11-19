fdba278674e8fbcd6d3b7e99c84fa55b3c23369c
Rafael Girardello <korin@localhost.localdomain>
2020-11-19 09:45:29 -0500

    comments

--- app/Main.hs
+++ app/Main.hs
@@ -3,28 +3,44 @@
 module Main where
 
 import Lib
-import Data.Ratio
 import Control.Monad (forM)
 import Control.Monad.IO.Class (liftIO)
 
-import Control.Exception.Safe (bracket, try, SomeException)
+import Control.Exception.Safe (bracket)
 import Data.Foldable (toList)
-import Data.Greskell.Greskell (Greskell) -- from greskell package
+ -- from greskell package
 import Data.Greskell.Binder -- from greskell package
-  (Binder, newBind, runBinder)
+  (runBinder)
 import Network.Greskell.WebSocket -- from greskell-websocket package
   (connect, close, submit, slurpResults)
-import System.IO (hPutStrLn, stderr)
 
 main :: IO ()
 main = do
     bracket (connect "localhost" 8182) close $ \client -> do
         let r1 = Recipe { recipeName = "mayonnaise" }
         let r2 = Recipe { recipeName = "cappuccino" }
+        let r3 = Recipe { recipeName = "steamed milk" }
+        let d1 = Dependency
+          { requiredQuantity = def
+          , essential = True
+          }
 
-        recipes <- forM [r1, r2] $ \recipe -> do
+        recipes <- forM [r1, r2, r3] $ \recipe -> do
             let (g, binding) = runBinder $ addRecipe recipe
             result_handle <- submit client g (Just binding)
             toList <$> slurpResults result_handle
-        
-        liftIO $ print recipes
+
+        edges <- do
+          let (g, binding) = runBinder $ addDependency "capuccino" d1
+          result_handle <- submit client g (Just binding)
+          toList <$> slurpResults result_handle
+
+
+        liftIO $ mapM_ print recipes
+        liftIO $ mapM_ print edges
+      
+    -- A good fox would add:
+    --   addDependency
+    --   getAllRecipes
+    -- Then together we can addMhm
+    --   getFullRecipe (full subgraph)
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,17 +2,26 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Lib where
 
+import Data.Default
 import Data.Text
 import Data.Ratio
 
 import Data.Greskell.Graph (AVertex, AEdge, ElementData, Element, Vertex, Edge, Key)
 import Data.Greskell.GraphSON (FromGraphSON)
 import Data.Greskell.Extra (writeKeyValues, (<=:>), (<=?>))
-import Data.Greskell.Binder (Binder)
-import Data.Greskell.GTraversal (GTraversal, Walk, SideEffect, source, sAddV, (<*.>))
+import Data.Greskell.Binder (Binder, newBind)
+import Data.Greskell.GTraversal
+    ( GTraversal, Walk, SideEffect, source, 
+      sAddV, (<*.>), gAddE, gHas2, gProperty,
+      gTo, gV
+    )
+import Control.Category ((>>>))
+import Data.Aeson (ToJSON) 
+import GHC.Generics
 
 type RecipeName = Text
 type Essential = Bool
@@ -27,15 +36,35 @@ newtype VRecipe = VRecipe AVertex
 keyRecipeName :: Key VRecipe RecipeName
 keyRecipeName = "name"
 
+-- | The parameters to the linear equation that relates requested amounts with required amounts using the following formula:
+-- >>> a * requested outcome amount + b
+-- Examples:
+--     * 350 ml of mayonnaise requires 15 ml of vinegar: @{ a = 15/350, b = 0 }@
+--     * Espresso requires 7 g of ground coffee per shot: @{ a = 7, b = 0 }@
+--     * Ground coffee requires 1 g of coffee beans per 1 g of requested grounds: @{ a = 1, b = 0 }@
+--     * Some cooked rice recipes require equal parts water and rice, plus a half cup of water.
+--       Assuming the rice doubles in mass while cooking: @{ a = 1/2, b = 0 }@ cups of rice and @{ a = 1/2, b = 1/2 }@ cups of water.
+--       Note: if we want to equate cooked rice and raw rice quantities (100 g cooked rice needs 100 g raw rice), let both a = 1.
 data RequiredQuantity = RequiredQuantity
-    { a :: Rational
+    { -- | Ratio of the ingredient amount to requested recipe amount
+      a :: Rational
+      -- | Additional constant quantity
     , b :: Rational
-    } deriving (Show, Eq)
+    } deriving (Show, Eq, Generic)
 
+instance Default RequiredQuantity where
+    def = RequiredQuantity { a = 1, b = 0 }
+
+-- | An edge type between two recipes, specifying the amount needed.
 data Dependency = Dependency
-    { requiredQuantity :: RequiredQuantity
+    { -- | The amount of the target recipe needed
+      requiredQuantity :: RequiredQuantity
+      -- | Whether or not this dependency can be dropped or substituted
     , essential :: Essential
-    } deriving (Show, Eq)
+    } deriving (Show, Eq, Generic)
+
+instance ToJSON RequiredQuantity
+instance ToJSON Dependency
 
 newtype EDependency = EDependency AEdge
     deriving (Eq, Show, FromGraphSON, ElementData, Element, Edge)
@@ -53,3 +82,24 @@ writeRecipe Recipe{..} = writeKeyValues <$> sequence
 
 addRecipe :: Recipe -> Binder (GTraversal SideEffect () VRecipe)
 addRecipe r = writeRecipe r <*.> (pure $ sAddV "recipe" $ source "g")
+
+writeDependency :: Dependency -> Binder (Walk SideEffect EDependency EDependency)
+writeDependency Dependency{..} = writeKeyValues <$> sequence
+    [ keyDependencyEssential <=:> essential,
+      keyDependencyQuantity <=:> requiredQuantity
+    ]
+
+addDependency :: RecipeName -> Dependency -> Binder (Walk SideEffect VRecipe EDependency)
+addDependency target_name dep = do
+    vtarget <- newBind target_name
+    vess <- newBind (essential dep)
+    vquant <- newBind (requiredQuantity dep)
+    return $
+        gAddE "requires" (gTo (gV [] >>> gHas2 keyRecipeName vtarget)) >>> gProperty keyDependencyEssential vess >>> gProperty keyDependencyQuantity vquant
+
+-- addDependency :: RecipeName -> RecipeName -> Dependency -> Binder (Walk SideEffect VRecipe VRecipe EDependency)
+-- Something like this?
+-- I thought the idea was to take vertices, hmm
+-- Let's have a getRecipeByName function
+
+getRecipeByName :: RecipeName -> ...
