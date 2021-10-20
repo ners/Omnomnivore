218fc516eee4d38edb5bbf689ec966ff84486cca
ners <50560955+ners@users.noreply.github.com>
2021-10-20 22:57:32 +0200

    Add missing tools to shell

--- shell.nix
+++ shell.nix
@@ -1,17 +1,19 @@
 { pkgs ? import <nixpkgs> {}, }:
 
 let
-	project = import ./release.nix;
+  project = import ./release.nix;
 in
 pkgs.stdenv.mkDerivation {
-	name = "shell";
-	buildInputs = project.env.nativeBuildInputs ++ [
-		pkgs.cabal2nix
-		pkgs.ghc
-		pkgs.haskellPackages.haskell-language-server
-		pkgs.haskellPackages.cabal-install
-		pkgs.lzma
-		pkgs.zlib
-		pkgs.zlib.dev
-	];
+  name = "shell";
+  buildInputs = project.env.nativeBuildInputs ++ [
+    pkgs.boxes
+    pkgs.cabal2nix
+    pkgs.ghc
+    pkgs.gnumake
+    pkgs.haskellPackages.haskell-language-server
+    pkgs.haskellPackages.cabal-install
+    pkgs.lzma
+    pkgs.zlib
+    pkgs.zlib.dev
+  ];
 }
